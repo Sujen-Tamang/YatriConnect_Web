7561422f666199188280e0c647e0bfe2c4282418
Sujen Tamang <np03cs4a230133@heraldcollege.edu.np>
2025-11-06 09:15:00 +0545

    feat(backend): implement role-based Admin and Super Admin setup

---
+++ Backend/models/userModel.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter your name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter your email'],
+    unique: true,
+    lowercase: true
+  },
+  phone: {
+    type: String,
+    required: [true, 'Please enter your phone number']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: 8,
+    select: false
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+  verificationCode: Number,
+  verificationCodeExpire: Date,
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  pendingEmail: String,
+    role: {
+    type: String,
+    enum: ['user', 'driver', 'admin'],
+    default: 'user'
+  },
+  savedRoutes: [
+    {
+      routeId: String,
+      from: String,
+      to: String,
+      name: String,
+      type: { type: String, enum: ['city', 'intercity'] }
+    }
+  ]
+}, { timestamps: true });
+
+export const User = mongoose.model('User', userSchema);
